911238a8247f55cd946e34e7b68e8269706e0afb
Guaguacoa <kevinsotelo141mail.com>
2024-08-26 07:06:18 -0500

    Primera version

---
+++ prueba.py
@@ -0,0 +1,22 @@
+digitos=(35,70,30,60,43,83,18,25,60,20,)
+uno=digitos[0]
+dos=digitos[1]
+tres=digitos[2]
+cuatro=digitos[3]
+cinco=digitos[4]
+seis=digitos[5]
+siete=digitos[6]
+ocho=digitos[7]
+nueve=digitos[8]
+diez=digitos[9]
+
+print (uno)
+print (dos)
+print (tres)
+print (cuatro)
+print (cinco)
+print (seis)
+print (siete)
+print (ocho)
+print (nueve)
+print (diez)
